d051e33ddcc42c9b118d015061652a3585e64194
chibiharu <chibiharujijimasa@gmail.com>
2022-04-10 02:40:57 +0900

    update：システム拡張に伴う構成変更

--- 03_materials/setup.sh
+++ 03_materials/setup.sh
@@ -15,29 +15,37 @@
 #####################################################################
 ## 事前設定
 #####################################################################
-# 現在の時刻を取得
-time=$(date '+%Y/%m/%d %T')
-
 # カレントパスを取得
-current_path=$(cd $(dirname $0); pwd)
+CPath=$(cd $(dirname $0); pwd)
 
 # 実行ログ出力先パス(デフォルト：カレントディレクトリ)
-SetUpLog=$crrent_path"/SetUpLog_"$time".log"
+SetUpLog=$CPath"/setuplog.log"
 
 
 #####################################################################
 ## 資材
 #####################################################################
+### Rootユーザか判定 ###
+function FuncRoot() {
+  echo "### Start function FuncRoot ###"
+  if [[ `whoami` != "root" ]]; then
+    echo "rootユーザで実行してください。"
+    exit 1
+  fi
+  echo "### End function FuncRoot ###"
+}
+
 ### 事前準備 ###
 function FuncPre() {
-  touch $SetUpLog
+  echo "### Start function FuncPre ###"
   apt-get update
   apt-get upgrade -y
+  echo "### End function FuncPre ###"
 }
 
 ### Python開発ツール ###
-# Python 3.9.4
 function FuncInstallPython() {
+  echo "### Start function FuncInstallPython ###"
   wget https://www.python.org/ftp/python/3.9.4/Python-3.9.4.tgz
   tar zxvf Python-3.9.4.tgz
   cd Python-3.9.4
@@ -45,36 +53,33 @@ function FuncInstallPython() {
   make
   make install
   python3.9 -v
+  echo "### End function FuncInstallPython ###"
 }
 
 ### Git ###
 # Git Latest
 function FuncInstallGit() {
+  echo "### Start function FuncInstallGit ###"
   apt-get install -y git
   git --version
+  echo "### End function FuncInstallGit ###"
 }
 
 ### Adafruit_Python_DHT ###
 function FuncCloneAdafruit() {
+  echo "### Start function FuncCloneAdafruit ###"
   git clone https://github.com/adafruit/Adafruit_Python_DHT.git
   cd Adafruit_Python_DHT/
   python setup.py install
-}
-
-### Rootユーザか判定 ###
-function FuncRoot() {
-  if [ ${EUID:-${UID}} = 0 ]; then
-      echo 'Try again as the Root user.'
-      exit 2
-  fi
+  echo "### End function FuncCloneAdafruit ###"
 }
 
 
 #####################################################################
 ## メイン処理
 #####################################################################
-FuncRoot | tee -a $SetUpLog
-FuncPre | tee -a $SetUpLog
-FuncInstallPython | tee -a $SetUpLog
-FuncInstallGit | tee -a $SetUpLog
-FuncCloneAdafruit | tee -a $SetUpLog
+FuncRoot >> $SetUpLog
+FuncPre >> $SetUpLog
+FuncInstallPython >> $SetUpLog
+FuncInstallGit >> $SetUpLog
+FuncCloneAdafruit >> $SetUpLog
